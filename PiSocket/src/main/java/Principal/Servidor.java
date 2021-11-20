a7a0adc0bced4a3056cab50ba5087eb57b6aa8eb
adrianpiveta <danielpiveta257@gmail.com>
2021-11-20 16:47:46 -0300

    Iniciação ao socket

---
+++ PiSocket/src/main/java/Principal/Servidor.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Principal;
+
+import java.io.IOException;
+import java.net.*;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author danie
+ */
+public class Servidor {
+    public static void main(String[] args) {
+        try {
+            ServerSocket socket = new ServerSocket(432);
+            System.out.println("Porta 432 aberta!");
+            System.out.println("Nova	conexão	com	o	cliente	"	+	
+				cliente.getInetAddress().getHostAddress());
+            Scanner	scanner	=	new	Scanner(cliente.getInputStream());
+            while	(scanner.hasNextLine())	{
+				System.out.println(scanner.nextLine());
+}
+        } catch (IOException ex) {
+            Logger.getLogger(Servidor.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+}
